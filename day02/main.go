fe18e7cc496c1197f7aeee9388346290209fce31
lim3nius <lessy.mot@gmail.com>
2022-12-05 00:18:16 +0100

    Day02

---
+++ day02/main.go
@@ -0,0 +1,92 @@
+package main
+
+import (
+	"fmt"
+	"os"
+	"strings"
+
+	cmn "github.com/Lim3nius/aoc2022/common"
+)
+
+const (
+	Rock     = "X"
+	Paper    = "Y"
+	Scissors = "Z"
+)
+
+var (
+	otherToMe = map[string]string{
+		"A": Rock,
+		"B": Paper,
+		"C": Scissors,
+	}
+
+	points = map[string]uint{
+		Rock:     1,
+		Paper:    2,
+		Scissors: 3,
+	}
+
+	winMap = map[string]string{
+		Rock:     Scissors,
+		Paper:    Rock,
+		Scissors: Paper,
+	}
+
+	meToWantRes = map[string]uint{
+		WinC:  Win,
+		DrawC: Draw,
+		LoseC: Lose,
+	}
+)
+
+const (
+	Win  = 6
+	Draw = 3
+	Lose = 0
+
+	LoseC = "X"
+	DrawC = "Y"
+	WinC  = "Z"
+)
+
+func eval(me, ot string) uint {
+	switch {
+	case me == ot:
+		return Draw
+	case ot == winMap[me]:
+		return Win
+	default:
+		return Lose
+	}
+}
+
+func ensureResult(op string, wantRes uint) uint {
+	for _, me := range []string{Rock, Paper, Scissors} {
+		if wantRes == eval(me, op) {
+			return wantRes + points[me]
+		}
+	}
+	return 0
+}
+
+func main() {
+	data := string(cmn.Must(os.ReadFile("input.txt")))
+	if data[len(data)-1] == '\n' {
+		data = data[:len(data)-1]
+	}
+
+	score := uint(0)
+	score2 := uint(0)
+	for _, ln := range strings.Split(data, "\n") {
+		pl := strings.Split(ln, " ")
+		ot, me := pl[0], pl[1]
+		ot = otherToMe[ot]
+
+		score += eval(me, ot) + points[me]
+		score2 += ensureResult(ot, meToWantRes[me])
+	}
+
+	fmt.Printf("Part1 -> %d\n", score)
+	fmt.Printf("Part2 -> %d\n", score2)
+}
